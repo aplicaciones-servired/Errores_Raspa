5f8b79794b61c4e53c2f0ac5d7837498695a3aad
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-09-09 10:03:01 -0500

    fix: corregir la ruta del archivo .env en la configuración de carga de entorno

--- Server/src/config/env.ts
+++ Server/src/config/env.ts
@@ -1,9 +1,9 @@
 import path from 'path'
 import dotenv from 'dotenv'
 
-export const envFile = path.resolve(__dirname, '..', '..', '.env')
+export const envFile = path.resolve(__dirname, '..', '.env')
 
-export const loadEnv = (): void => {
+const loadEnv = (): void => {
   dotenv.config({ path: envFile })
 }
 
